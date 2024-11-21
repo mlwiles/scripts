6daddf985b5a59dc416a22aba9b075f0648be5f9
Mike Wiles <mike.lee.wiles@gmail.com>
2024-11-20 21:09:04 -0500

    cwe 4.16

--- perl/cwe/cwe.xml.pl
+++ perl/cwe/cwe.xml.pl
@@ -83,7 +83,7 @@ sub sanitize {
 
 # https://cwe.mitre.org/data/archive.html
 #my $filename = 'cwec_v4.6.xml';
-my $filename = 'cwec_v4.15.xml';
+my $filename = 'cwec_v4.16.xml';
 
 #2dim array - key (CWEID) - data (NAME, DESC) 
 my %CWEData = {};
